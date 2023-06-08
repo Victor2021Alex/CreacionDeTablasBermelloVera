2f016e774c9517409aca6415b82dfc6aca2f3e26
Victor <victorbermello61@gmail.com>
2023-06-08 18:06:56 -0500

    Agregar archivos de proyecto.

---
+++ CreacionDeTablasBermelloVera/Models/AplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CreacionDeTablasBermelloVera.Models
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ CreacionDeTablasBermelloVera/Models/Genero.cs
@@ -0,0 +1,9 @@
+﻿namespace CreacionDeTablasBermelloVera.Models
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string estado { get; set; }
+    }
+}
